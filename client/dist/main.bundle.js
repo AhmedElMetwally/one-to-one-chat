af9b5637f6da1542ddb3ae18c29e293c5d928fd7
Ahmed <carawanbaik77@gmail.com>
2018-01-09 16:06:51 +0200

    fix error use -> ng build --env=prod

--- client/dist/main.bundle.js
+++ client/dist/main.bundle.js
@@ -878,13 +878,9 @@ var UserService = (function () {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
 var environment = {
-    production: false,
-    url: 'http://localhost:3000'
+    production: true,
+    url: ''
 };
 
 
